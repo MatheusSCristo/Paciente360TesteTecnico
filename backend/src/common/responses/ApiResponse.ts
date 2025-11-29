a36cc5eb1703cefb56a30ded76b6cd78dd962607
MatheusSCristo <cristo.matheus@outlook.com>
2025-11-28 22:13:12 -0300

    feat(backend): adicionar classe de resposta padrão de sucesso da api

---
+++ backend/src/common/responses/ApiResponse.ts
@@ -0,0 +1,36 @@
+export interface ApiPaginationMeta {
+  page: number;
+  total: number;
+  perPage?: number;
+  totalPages?: number;
+}
+
+export class ApiResponse<T> {
+  message: string;
+  data?: T;
+  timestamp: string;
+  meta?: Record<string, any>;
+
+  private constructor(message: string, data?: T, meta?: Record<string, any>) {
+    this.message = message;
+    this.data = data;
+    this.meta = meta;
+    this.timestamp = new Date().toISOString();
+  }
+
+  static success<T>(
+    data: T,
+    message: string = 'Operação realizada com sucesso',
+  ): ApiResponse<T> {
+    return new ApiResponse(message, data);
+  }
+
+  // 2. Cenário de Lista Paginada
+  static successPaginated<T>(
+    data: T,
+    meta: ApiPaginationMeta,
+    message: string = 'Lista recuperada',
+  ): ApiResponse<T> {
+    return new ApiResponse(message, data, meta);
+  }
+}
